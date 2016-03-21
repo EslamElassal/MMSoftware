20d985ba5152d7f513fdcc6c7de4f88e47eadd26
Hisham Galal <hisham.galal@fci.au.edu.eg>
2016-03-21 13:01:09 +0200

    Moved PrintHello to start of the class

--- CS381Project/src/Example.java
+++ CS381Project/src/Example.java
@@ -1,14 +1,15 @@
 
 public class Example {
 
-	public static void main(String[] args) {
-		PrintHello();
-
-	}
 	public static void PrintHello()
 	{
 		//This method prints hello world to user
 		System.out.println("Hello World");
 	}
+	public static void main(String[] args) {
+		PrintHello();
+
+	}
+	
 
 }
