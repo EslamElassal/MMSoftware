bac1e90c77a1ed7c0ebe8c347d313a41f4b705d6
Hisham Galal <hisham.galal@fci.au.edu.eg>
2016-03-21 13:22:09 +0200

    Added PrintHelloName

--- CS381Project/src/Example.java
+++ CS381Project/src/Example.java
@@ -1,14 +1,22 @@
 
+import java.util.*;
 public class Example {
 
 	public static void main(String[] args) {
-		PrintHello();
+		PrintHelloName();
 
 	}
 	public static void PrintHello()
 	{
 		//This method prints hello world to user
 		System.out.println("Hello World");
+	}
+	public static void PrintHelloName()
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter Name: ");;
+		String name=s.nextLine();
+		System.out.printf("Hello %s\n",name);
 	}
 
 }
